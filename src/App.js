d8054968b74b425f448c7131ae90bdec23e44b9d
junes <junes3754@gmail.com>
2021-01-12 22:22:48 +0900

    Component 와 Props 알아보기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+function Food({fav}){
+// function Food(props){
+    return(<h3>I like {fav}</h3>)
+    // return(<h3>I like {props.fav}</h3>)
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+        <Food fav={"kimchi"} />
+        <Food fav={"ramen"} />
+        <Food fav={"삼겹살"} />
+        <Food fav={"닭갈비"} />
     </div>
   );
 }
